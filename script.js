f465d0b22f8395565c4c080f33ef5661df603752
Samuel263 <88627774+Samuel263@users.noreply.github.com>
2024-07-21 23:08:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const products = {
         'website': [
             { id: 'PW', name: 'Página Web Informativa: Portafolio de 10 imágenes + elaboración de la Misión y la Visión de tu negocio + Dominio(www.name.cl) + Hosting Anual en 48 Horas', price: 149.99, image: 'assets/images/image1.jpg', tags: ['rojo', 'rosas', 'ligero', 'Rosatel'] },
-            { id: 'ACC6R-6G-HIP', name: '"Registro de Usuarios" Login y Acceso con Google', price: 30.00, image: 'assets/images/image1.jpg', tags: ['amarillo', 'girasoles', 'mediano', 'Rosatel'] },
+            { id: 'ACC6R-6G-HIP', name: '"Registro de Usuarios" Login y Acceso con Google', price: 29.99, image: 'assets/images/image1.jpg', tags: ['amarillo', 'girasoles', 'mediano', 'Rosatel'] },
             { id: 'ACC-6R-GER-FLO', name: '"Registro de Usuarios" Login y Acceso con Cuenta Personalizada', price: 5.99, image: 'assets/images/image1.jpg', tags: ['blanco', 'rosado', 'flores', 'pesado', 'Rosatel'] },
              { id: 'FR-100-COR', name: 'Botón de Whatsapp Flotante con Mensaje Personalizado de Empresa', price: 2.99, image: 'assets/images/image1.jpg', tags: ['amarillo', 'chocolate', 'dulce', 'cacao', 'ferrero'] },
             { id: 'CHOC-CBCL-120', name: 'Chatbot con Inteligencia Artificial Interacción con Clientes 10 Preguntas', price: 3.99, image: 'assets/images/image1.jpg', tags: ['rojo', 'chocolate', 'amargo', 'cacao', 'bombones'] },
